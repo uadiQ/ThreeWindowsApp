5a82f1d53de3141220f33ff42deb91f386e39a84
Vadym Shoshin <shoshin.vadik@gmail.com>
2017-10-20 00:16:29 +0300

    Reduced animation time

--- ThreeWindowsApp/UIButton+Shrink+Extension.swift
+++ ThreeWindowsApp/UIButton+Shrink+Extension.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIButton
 {
     func shrink(){
-        UIView.animate(withDuration: 0.2, delay: 0, options: [.autoreverse], animations:
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.autoreverse], animations:
             {
                 self.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
             })
